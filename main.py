46ab2da25a9b201a399a7b5532bea993c06988f1
codycode <cjthomp007@gmail.com>
2026-06-30 02:30:54 -0600

    Gave pilot controls, put pilots life in theoretical danger

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroid_field import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    dt = 0.0
+
+    
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+
+        updatable.update(dt)
+
         screen.fill("black")
+        
+        for drawn in drawable:
+            drawn.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000 
+
 
 
 if __name__ == "__main__":
